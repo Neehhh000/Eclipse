998b59e6ca47bbf6674ae63625128a8dc54bc437
italob221 <italotiakao12@gmail.com>
2025-12-01 20:50:33 -0300

    Update personagens.php

---
+++ personagens.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Cadastro</title>
+</head>
+<body>
+    <header>
+        <h1>PERSONAGENS</h1>
+
+ 
+ 
+<section class="section3">
+    <div class="personagem">
+        <h2>JUAN</h2>
+        <a href="personagens.php"><img class="imagens-personagem" src="img/6.png"></a>
+    </div>
+    <div class="personagem">
+        <h2>ÍTALO</h2>
+        <a href="personagens.php"><img class="imagens-personagem" src="img/7.png"></a>
+    </div>
+    <div class="personagem">
+        <h2>KAUÃ</h2>
+        <a href="personagens.php"><img class="imagens-personagem" src="img/8.png"></a>
+    </div>
+</section>
+ 
+Ítalo
+ 
+Kauã
+ 
+<h2>SOBRE O JOGO</h2>
+ 
+Num mundo Eclipse Final, reinos esquecidos lutam pela última luz — aventure-se e descubra segredos que moldam o destino.
+         
+Eclipse Final é um jogo Roguelike aonde você escolhe seu próprio personagem. Explore o mundo enquanto se diverte encontrando diversos equipamentos e melhorias para seu personagem.
+ 
+Erga se e se torne o heroi desse mundo.
+ 
+    <div>
+      &copy; <?php echo date('Y'); ?> Eclipse Final
+    </div>
+</body>
+</html>
