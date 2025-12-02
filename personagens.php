af451ec4ca3efb85184686934eed0a4b593ad61e
Neehhh000 <kauaassis80808@gmail.com>
2025-12-02 00:40:19 -0300

    Add files via upload

---
+++ personagens.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Cadastro</title>
+</head>
+
+<br>
+    <a href="#" id="inicio"></a>
+<br>
+
+<body>
+    <header>
+        <h1>PERSONAGENS</h1>
+
+        <br>
+
+        <nav class="menu">
+            <a href="index.php">Página inicial</a>
+            <a href="cadastro.php">Cadastro</a>
+            <a href="dados.php">Cadastrados</a>
+        </nav>
+    </header>
+
+        <section class="section3">
+            <div class="linha-personagem">
+                <img class="imagens-personagem2" src="img/6.png">
+                <div class="texto-personagem">
+                    <h2>Juan</h2>
+                    <p class="texto-personagem">
+                        Juan nasceu na miséria e cresceu entre violência, fome e o triste ciclo de pobre contra pobre. Forjado pela dor, escolheu agir nas sombras sem jamais apagar a luz em seu coração. Em nome da paz, tornou-se o ceifador daqueles que oprimiam o próprio povo — um justiceiro silencioso, temido pelos tiranos e amado pelos esquecidos.
+                    </p>
+                </div>
+            </div>
+
+            <div class="linha-personagem">
+                <img class="imagens-personagem2" src="img/7.png">
+                <div class="texto-personagem">
+                    <h2>Ítalo</h2>
+                    <p class="texto-personagem">
+                        Criado no campo, o jovem guerreiro Ítalo jamais esqueceu a visão da própria vila sendo tomada por monstros espreitadores. Salvo pelos guerreiros do reino, jurou tornar-se tão grandioso quanto eles.
+                        
+                        Agora, empunhando a espada contra qualquer ameaça e erguendo o escudo para proteger os inocentes, Ítalo avança como a muralha que um dia desejou ter ao seu lado.
+                    <p>
+                </div>
+            </div>
+
+            <div class="linha-personagem">
+                <img class="imagens-personagem2" src="img/8.jpg">
+                <div class="texto-personagem">
+                    <h2>Kauã</h2>
+                    <p class="texto-personagem">
+                        Kauã descende de uma prestigiada família nobre de magos do reino. Movido por um amor profundo pela magia, transformou-se em um estudioso incansável, sempre em busca de novos conhecimentos. Após anos vivendo sob a proteção da nobreza, percebeu que as artes mais extraordinárias e misteriosas não estavam ali, mas além da muralha — onde a verdadeira magia o aguardava.
+                    </p>
+                </div>
+            </div>
+
+            <div class="linha-personagem">
+                <img class="imagens-personagem2" src="img/9.jpg">
+                <div class="texto-personagem">
+                    <h2>Poliana</h2>
+                    <p class="texto-personagem">
+                        Poliana nasceu e cresceu sob a proteção da floresta, que a moldou como filha e caçadora. Longe da civilização, tornou-se uma lenda viva, dona de uma mira que jamais falha.
+                        Quando criaturas do abismo ameaçaram a paz de seu lar, ela ergueu o arco pela primeira — e única — vez em nome da humanidade. Agora, guiada pelo espírito da própria mata, Poliana enfrenta qualquer sombra que ouse cruzar seu caminho.
+                    </p>
+                </div>
+        </section>
+
+    <div class="botao-inicio">
+        <a href="#inicio">Voltar para o começo</a>
+    </div>
+
+    <footer>
+        <div>
+            <h5>Todos os direitos reservados &copy; <?php echo date('Y'); ?> Eclipse Final</h5>
+        </div>
+    </footer>
+</body>
+</html>
